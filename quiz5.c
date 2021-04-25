542fc747e628a68d21d944b4d90e2ccc147b8349
ooohwen <83047857+ooohwen@users.noreply.github.com>
2021-04-26 02:14:52 +0800

    Update quiz5.c

--- quiz5.c
+++ quiz5.c
@@ -1,16 +1,20 @@
 #include<stdio.h>
 
 int wordCount(char *);
-int isLetter(char c){
-	if(c>='A'&& c<='Z' || c>='a'&& c<='z'){
+int isLetter(char c)
+{
+	if(c>='A'&& c<='Z' || c>='a'&& c<='z')
+	{
 		return 1;
 	}
-	else{
+	else
+	{
 		return 0;
 	}
 }
 
-int main(){
+int main()
+{
 	//declare
 	char s[]="There are 6 words in the sentence.";
 	int sentence; //pointer
@@ -26,12 +30,12 @@ int wordCount(char *sentence){
 	//count words in the sentence
 	int i, count=0;
 	
-	for(i = 0; sentence[i] != '\0'; i++){
-		
-		if(isLetter(sentence[i])&&!isLetter(sentence[i+1])){
-	
+	for(i = 0; sentence[i] != '\0'; i++)
+	{	
+		if(isLetter(sentence[i]) && !isLetter(sentence[i+1]))
+		{
 			count++;
-			}
+		}
 		
 	}
 	return count;
